35c5c108557b293390fe1b5a15104d2af5241496
Ruslan <110040060+Deadusik@users.noreply.github.com>
2024-01-16 21:39:06 +0200

    add mutations methods

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,17 +1,37 @@
+import { IPost } from "../models/IPost"
 import { postAPI } from "../services/PostService"
 import { PostItem } from "./PostItem"
 
 export const PostContainer = () => {
-    const { data: posts, isLoading, error } = postAPI.useFetchAllUsersQuery(5)
+    const { data: posts, isLoading, error } = postAPI.useFetchAllUsersQuery(100)
+
+    const [createPost, { error: createError, isLoading: isCreateLoading }] = postAPI.useCreatePostMutation()
+    const [removePost, { error: removeError, isLoading: isRemoveLoading }] = postAPI.useDeletePostMutation()
+    const [updatePost, { error: updateError, isLoading: isUpdateLoading }] = postAPI.useUpdatePostMutation()
+
+    const handleCreate = async () => {
+        const title = prompt()
+        await createPost({ title, body: title } as IPost)
+    }
+
+    const handleRemove = (post: IPost) => {
+        removePost(post)
+    }
+
+    const handleUpdate = (post: IPost) => {
+        updatePost(post)
+    }
+
     return (
         <div>
+            <button onClick={handleCreate}>Add new post</button>
             <div>
                 {
                     isLoading ?
                         <h1>Loading</h1>
                         :
                         posts?.map(post =>
-                            <PostItem key={post.id} post={post} />)
+                            <PostItem key={post.id} post={post} remove={handleRemove} update={handleUpdate} />)
                 }
                 {
                     error && <h1>Loding data error</h1>
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,17 +1,28 @@
 import { FC } from "react"
 import { IPost } from "../models/IPost"
-import { postAPI } from "../services/PostService"
 
 interface IPostItemProps {
-    post: IPost
+    post: IPost,
+    remove: (post: IPost) => void
+    update: (post: IPost) => void
 }
 
-export const PostItem: FC<IPostItemProps> = ({ post }) => {
+export const PostItem: FC<IPostItemProps> = ({ post, remove, update }) => {
+
+    const handleRemove = (event: React.MouseEvent) => {
+        event.stopPropagation()
+        remove(post)
+    }
+
+    const handleUpdate = (event: React.MouseEvent) => {
+        const title = prompt() || ''
+        update({ ...post, title })
+    }
 
     return (
-        <div className="post">
+        <div className="post" onClick={handleUpdate}>
             {post.id}. {post.title}
-            <button>DELETE</button>
+            <button onClick={handleRemove}>DELETE</button>
         </div>
     )
 }
